53b8686d9577cfc4db739940be4351c4d7588e4f
hube12 <h66R10@gmail.com>
2018-08-30 18:04:16 +0200

    Add files via upload
    revert bad changes, and changed how permissions_for require a member instance instead of a simple role

--- Yunbot.py
+++ Yunbot.py
@@ -1,68 +1,44 @@
-import discord
-import bot_utils
-
 import asyncio
-import re
+import logging
+import time
 
-def create_yunbot():
-    client = discord.Client()
+import counting_check_bot
+import Yunbot
 
-    with open("access_tokens/yunbot.txt") as f:
-        client.access_token = f.read()
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
-    @client.async_event
-    async def on_error(event, *args, **kwargs):
-        await bot_utils.on_error(client, event, *args, **kwargs)
-    
-    @client.async_event
-    async def on_ready():
-        print("YUNBOT")
-        print('Logged in as')
-        print(client.user.name)
-        print(client.user.id)
-        print('------')
+counting_check = counting_check_bot.create_counting_check_bot()
+yunbot = Yunbot.create_yunbot()
 
-    @client.async_event
-    async def on_message(message):
-        if not message.author.bot:
+bots = [
+    counting_check,
+    yunbot
+]
 
-            read_messages_perm = message.channel.permissions_for(message.server.default_role if message.server else client.user).read_messages
-            if not (read_messages_perm == None or read_messages_perm == True):
-                return
-            
-            if message.author.id == '310231043762290700': # Acheron
-                if '\u2764' in message.content:
-                    for emoji in ['\U0001F494', '\U0001F498', '\U0001F499', '\U0001F49A', '\U0001F49C', '\U0001F49D', '\U0001F49B', '\U0001F5A4', '\U0001F60D', '\U0001F618']:
-                        await client.add_reaction(message, emoji)
+loop = asyncio.get_event_loop()
 
-            
-            person = None
-            sentences = re.split(r'[\.,!\?:;]', message.content)
-
-            for sentence in sentences:
-                words = sentence.split(" ")
-                imIdx = -1
-                for i, word in enumerate(words):
-                    word = word.lower()
-                    if (word in ['im', 'i\'m', 'i’m', 'i‘m', 'i`m'] or (i != 0 and word == 'am' and words[i - 1].lower() == 'i')):
-                        if i != len(words) - 1:
-                            imIdx = i
-
-                if imIdx != -1:
-                    person = " ".join(words[(imIdx+1):])
-
-            if person != None:
-                if person.lower() == 'yun':
-                    yun = 'No, im yun, you\'re ' + message.author.name + '!'
-                else:
-                    yun = 'Hi ' + person + ', im yun!'
-
-                if not message.author.server_permissions.mention_everyone:
-                    if "@everyone" in yun:
-                        return
-                    elif "@here" in yun:
-                        return
-                
-                await client.send_message(message.channel, yun)
-
-    return client
+try:
+    while True:
+        try:
+            loop.run_until_complete(asyncio.gather(*[bot.start(bot.access_token) for bot in bots]))
+        except KeyboardInterrupt:
+            raise
+        except:
+            logger.info("Exception occurred, assuming disconnect, waiting 1 minute...")
+            time.sleep(60)
+except KeyboardInterrupt:
+    loop.run_until_complete(asyncio.gather(*[bot.logout() for bot in bots]))
+    pending = asyncio.Task.all_tasks()
+    gathered = asyncio.gather(*pending)
+    try:
+        gathered.cancel()
+        loop.run_until_complete(gathered)
+        gathered.exception()
+    except:
+        pass
+finally:
+    loop.close()
